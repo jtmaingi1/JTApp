5d2b610289b40479842b3fbc95907b2691727aa1
Jeff Maingi <jtmaingi@gmail.com>
2018-09-14 01:40:18 +0300

    Changes on JS

--- public/js/plugins.js
+++ public/js/plugins.js
@@ -286,19 +286,7 @@ URL: https://themeforest.net/user/ahmedbeheiry
 				_url: $(contactForm).attr('action'),
 
 				data: "name=" + name + "&mail=" + mail + "&message=" + message,
-				 beforeSend: function(text) {
-			 	submitBtn.html("Sending...");
-			 	formResponse.fadeOut(200).text("");
-			 },
-				success: function (text) {
-					if(text == "success") {
-						contactForm[0].reset();
-						formResponse.text("Thanks! Your message sent correctly.").fadeIn(1000);
-						submitBtn.html("Send Message");
-					} else {
-						formResponse.text(text).fadeIn(1000);
-					}
-				}
+		
 			})
 			.done(function(response) {
 				// Make sure that the formMessages div has the 'success' class.
