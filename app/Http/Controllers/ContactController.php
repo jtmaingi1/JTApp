ebf7d185bec73cde134821ef63dc75adc2dd87de
Jeff Maingi <jtmaingi@gmail.com>
2018-09-27 02:55:24 +0300

    Changes to form

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -29,7 +29,7 @@ class ContactController extends Controller
 				$name = str_replace(array("\r","\n"),array(" "," "),$name);
         $email = filter_var(trim($_POST["mail"]), FILTER_SANITIZE_EMAIL);
         $message = trim($_POST["message"]);
-
+        $phone = trim($_POST["phone"]);
         // Check that data was sent to the mailer.
         if ( empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
             // Set a 400 (bad request) response code and exit.
@@ -43,12 +43,14 @@ class ContactController extends Controller
         $recipient = "info@jtmaingi.com";
 
         // Set the email subject.
-        $subject = "New contact from $name";
+        $subject = "New Message from $name";
 
         // Build the email content.
         $email_content = "Name: $name\n";
+        $email_content .= "Phone Number: $phone\n";
         $email_content .= "Email: $email\n\n";
         $email_content .= "Message:\n$message\n";
+
 
         // Build the email headers.
         $email_headers = "From: $name <$email>";
